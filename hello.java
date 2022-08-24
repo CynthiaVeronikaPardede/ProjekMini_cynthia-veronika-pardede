5aef04047fa36c6fd7bc0be2f53cabec88268fcc
Cynthia Pardede <62656001+Html19@users.noreply.github.com>
2022-08-24 18:16:57 +0700

    add yey

--- hello.java
+++ hello.java
@@ -2,8 +2,8 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Hello, Kim Namjoon");
+        System.out.println("Hello, Kim Seojin");
         System.out.println("Hello, Suga");
         System.out.println("Hello, jhope");
-        System.out.println("Hello, Kim Seojin");
     }
 }
